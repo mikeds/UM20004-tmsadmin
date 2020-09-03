a222b87a80f99bb68a07b247daebc214debd0edc
Marknel Pogi <marknel.pineda23@gmail.com>
2020-09-04 06:44:31 +0800

    Added: Merchant + Merchant Account, Added: Initial setup income scheme and income scheme type

--- application/config/routes.php
+++ application/config/routes.php
@@ -59,6 +59,11 @@ $route["admin-accounts/(:num)"]         = "admin/Admin_accounts/index/$1";
 $route["admin-accounts/new"]            = "admin/Admin_accounts/new";
 $route["admin-accounts/update/(:any)"]  = "admin/Admin_accounts/update/$1";
 
+$route["merchant-accounts"]                = "admin/Merchant_accounts";
+$route["merchant-accounts/(:num)"]         = "admin/Merchant_accounts/index/$1";
+$route["merchant-accounts/new"]            = "admin/Merchant_accounts/new";
+$route["merchant-accounts/update/(:any)"]  = "admin/Merchant_accounts/update/$1";
+
 $route['top-up-otc']    = "admin/Top_up_otc";
 $route['encash-otc']    = "admin/Encash_otc";
 
--- application/modules/admin/views/merchants/form.php
+++ application/modules/admin/views/merchants/form.php
@@ -23,20 +23,41 @@
 								</div>
 							</div>
 							<div class="row">
-								<div class="col-xl-8">
+								<div class="col-xl-4">
 									<div class="form-group">
-										<label>Business Name <span class="text-danger">*</span></label>
-										<input name="business-name" class="form-control" placeholder="Business Name" value="<?=isset($post['business-name']) ? $post['business-name'] : ""?>">
-										<span class="text-danger"><?=form_error('business-name')?></span>
+										<label>First Name <span class="text-danger">*</span></label>
+										<input name="first-name" class="form-control" placeholder="First Name" value="<?=isset($post['first-name']) ? $post['first-name'] : ""?>">
+										<span class="text-danger"><?=form_error('first-name')?></span>
+									</div>
+								</div>
+								<div class="col-xl-4">
+									<div class="form-group">
+										<label>Middle Name </label>
+										<input name="middle-name" class="form-control" placeholder="Middle Name" value="<?=isset($post['middle-name']) ? $post['middle-name'] : ""?>">
+										<span class="text-danger"><?=form_error('middle-name')?></span>
+									</div>
+								</div>
+								<div class="col-xl-4">
+									<div class="form-group">
+										<label>Last Name <span class="text-danger">*</span></label>
+										<input name="last-name" class="form-control" placeholder="Last Name" value="<?=isset($post['last-name']) ? $post['last-name'] : ""?>">
+										<span class="text-danger"><?=form_error('last-name')?></span>
 									</div>
 								</div>
 							</div>
 							<div class="row">
-								<div class="col-xl-12">
+								<div class="col-xl-4">
 									<div class="form-group">
-										<label>Business Address</label>
-										<input name="address" class="form-control" placeholder="Business Address" value="<?=isset($post['address']) ? $post['address'] : ""?>">
-										<span class="text-danger"><?=form_error('address')?></span>
+										<label>Gender <span class="text-danger">*</span></label>
+										<?=isset($gender) ? $gender : ""?>
+										<span class="text-danger"><?=form_error('gender')?></span>
+									</div>
+								</div>
+								<div class="col-xl-4">
+									<div class="form-group">
+										<label>Date of Birth <span class="text-danger">*</span></label>
+										<input name="dob" type="date" class="form-control" value="<?=isset($post['dob']) ? $post['dob'] : ""?>">
+										<span class="text-danger"><?=form_error('dob')?></span>
 									</div>
 								</div>
 							</div>
@@ -50,9 +71,9 @@
 								</div>
 								<div class="col-xl-4">
 									<div class="form-group">
-										<label>Contact Person </label>
-										<input name="contact-person" class="form-control" placeholder="Contact Person" value="<?=isset($post['contact-person']) ? $post['contact-person'] : ""?>">
-										<span class="text-danger"><?=form_error('contact-person')?></span>
+										<label>Mobile No. </label>
+										<input name="mobile-no" class="form-control" placeholder="Mobile No." value="<?=isset($post['mobile-no']) ? $post['mobile-no'] : ""?>">
+										<span class="text-danger"><?=form_error('mobile-no')?></span>
 									</div>
 								</div>
 								<div class="col-xl-4">
@@ -60,6 +81,52 @@
 										<label>Contact No. </label>
 										<input name="contact-no" class="form-control" placeholder="Contact No." value="<?=isset($post['contact-no']) ? $post['contact-no'] : ""?>">
 										<span class="text-danger"><?=form_error('contact-no')?></span>
+									</div>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-xl-4">
+									<div class="form-group">
+										<label>House No./ Unit No. / Building <span class="text-danger">*</span></label>
+										<input name="address" class="form-control" placeholder="House No./ Unit No. / Building" value="<?=isset($post['address']) ? $post['address'] : ""?>">
+										<span class="text-danger"><?=form_error('address')?></span>
+									</div>
+								</div>
+								<div class="col-xl-4">
+									<div class="form-group">
+										<label>Street <span class="text-danger">*</span></label>
+										<input name="street" class="form-control" placeholder="Street" value="<?=isset($post['street']) ? $post['street'] : ""?>">
+										<span class="text-danger"><?=form_error('street')?></span>
+									</div>
+								</div>
+								<div class="col-xl-4">
+									<div class="form-group">
+										<label>Barangay </label>
+										<input name="brgy" class="form-control" placeholder="Barangay" value="<?=isset($post['brgy']) ? $post['brgy'] : ""?>">
+										<span class="text-danger"><?=form_error('brgy')?></span>
+									</div>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-xl-4">
+									<div class="form-group">
+										<label>City <span class="text-danger">*</span></label>
+										<input name="city" class="form-control" placeholder="City" value="<?=isset($post['city']) ? $post['city'] : ""?>">
+										<span class="text-danger"><?=form_error('city')?></span>
+									</div>
+								</div>
+								<div class="col-xl-4">
+									<div class="form-group">
+										<label>Country <span class="text-danger">*</span></label>
+										<?=isset($country) ? $country : ""?>
+										<span class="text-danger"><?=form_error('country')?></span>
+									</div>
+								</div>
+								<div class="col-xl-4">
+									<div class="form-group">
+										<label>Provice/Region <span class="text-danger">*</span></label>
+										<?=isset($province) ? $province : ""?>
+										<span class="text-danger"><?=form_error('province')?></span>
 									</div>
 								</div>
 							</div>

402fc21388c7074907c180112173548240ba7df9
Marknel Pogi <marknel.pineda23@gmail.com>
2020-09-24 23:10:38 +0800

    Added: Ledger Admin - Pagination, Added: Ledger Admin - Search

--- application/modules/admin/controllers/Ledger.php
+++ application/modules/admin/controllers/Ledger.php
@@ -13,12 +13,61 @@ class Ledger extends Admin_Controller {
 		$this->_admin_account_data = $this->get_account_data();
 	}
 
-	public function index() {
+	public function index($page = 1) {
 		$account_results            = $this->_admin_account_data['results'];
         $admin_oauth_bridge_id		= $account_results['admin_oauth_bridge_id'];
         $account_oauth_bridge_id    = $account_results['oauth_bridge_id'];
 		$wallet_address             = $account_results['wallet_address'];
 		
+		$this->_data['form_url']		= base_url() . "ledger";
+		$this->_data['notification'] 	= $this->session->flashdata('notification');
+
+		$redirect_url					= "";
+
+		$txid	= isset($_GET['txid']) ? ($_GET['txid'] ? $_GET['txid'] : "" ) : "";
+		$refid	= isset($_GET['refid']) ? ($_GET['refid'] ? $_GET['refid'] : "" ) : "";
+
+		$from 	= isset($_GET['from']) ? ($_GET['from'] ? $_GET['from'] : "" ) : "";
+		$to		= isset($_GET['to']) ? ($_GET['to'] ? $_GET['to'] : "" ) : "";
+
+		$txamount = isset($_GET['txamount']) ? (is_numeric($_GET['txamount']) ? $_GET['txamount'] : "" ) : "";
+
+		$sort	= isset($_GET['sort']) ? ($_GET['sort'] ? $_GET['sort'] : "ASC" ) : "ASC";
+		$sort	= $sort == "ASC" ? $sort : "DESC";
+
+		$post = array();
+
+		if ($_POST) {
+			$txid	= $this->input->post('tx-id');
+			$refid	= $this->input->post('ref-id');
+	
+			$from 	= $this->input->post('from');
+			$to		= $this->input->post('to');
+	
+			$txamount = $this->input->post('amount');
+	
+			$sort	= $this->input->post('sort');
+			$sort	= $sort == "ASC" ? $sort : "DESC";
+		}
+
+		$this->_data['sort'] = $this->generate_selection(
+			"sort", 
+			array(
+				array(
+					'id'	=> 'ASC',
+					'name'	=> 'Ascending'
+				),
+				array(
+					'id'	=> 'DESC',
+					'name'	=> 'Decending'
+				)
+			), 
+			$sort, 
+			"id", 
+			"name", 
+			true
+		);
+
 		$select = array(
 			'transaction_id as "TX ID"',
 			'transaction_sender_ref_id as "Sender Ref ID"',
@@ -36,6 +85,109 @@ class Ledger extends Admin_Controller {
 			'ledger_datum_bridge_id'	=> $admin_oauth_bridge_id
 		);
 
+
+		if ($from != "") {
+			if ($from != "" && $to == "" && validate_date($from)) {
+				$where = array_merge(
+					$where,
+					array(
+						'DATE(ledger_datum_date_added) >=' => $from
+					)
+				);
+
+				$post = array_merge(
+					$post,
+					array(
+						'from'	=> $from
+					)
+				);
+
+				$redirect_url = $redirect_url == "" ? "?from={$from}" : "";
+			} else if ($from != "" && $to != "" && validate_date($from) && validate_date($to) && (strtotime($from) <= strtotime($to))) {
+				$where = array_merge(
+					$where,
+					array(
+						'DATE(ledger_datum_date_added) >=' => $from,
+						'DATE(ledger_datum_date_added) <=' => $to,
+					)
+				);
+
+				$post = array_merge(
+					$post,
+					array(
+						'from'	=> $from,
+						'to'	=> $to,
+					)
+				);
+
+				$redirect_url = $redirect_url == "" ? "?from={$from}&to={$to}" : "";
+			}
+		}
+
+		if ($txid != "") {
+			$where = array_merge(
+				$where,
+				array(
+					'tx_id'	=> $txid
+				)
+			);
+
+			$post = array_merge(
+				$post,
+				array(
+					'tx-id'	=> $txid
+				)
+			);
+
+			$redirect_url = $redirect_url == "" ? "?txid={$txid}" : $redirect_url . "&txid={$txid}";
+		}
+
+		if ($refid != "") {
+			$where = array_merge(
+				$where,
+				array(
+					'transaction_sender_ref_id'	=> $refid
+				)
+			);
+
+			$post = array_merge(
+				$post,
+				array(
+					'ref-id'	=> $refid
+				)
+			);
+
+			$redirect_url = $redirect_url == "" ? "?refid={$refid}" : $redirect_url . "&refid={$refid}";
+		}
+
+		if ($txamount != "") {
+			$where = array_merge(
+				$where,
+				array(
+					'transaction_amount'	=> $txamount
+				)
+			);
+
+			$post = array_merge(
+				$post,
+				array(
+					'amount'	=> $txamount
+				)
+			);
+
+			$redirect_url = $redirect_url == "" ? "?txamount={$txamount}" : $redirect_url . "&txamount={$txamount}";
+		}
+
+		$this->_data['post'] = $post;
+
+		if ($_POST) {
+			$redirect_url = $redirect_url == "" ? "?sort={$sort}" : $redirect_url . "&sort={$sort}";
+
+			if ($redirect_url != "") {
+				redirect($this->_data['form_url'] . $redirect_url);
+			}
+		}
+
 		$inner_joints = array(
 			array(
 				'table_name'	=> 'transactions',
@@ -46,6 +198,12 @@ class Ledger extends Admin_Controller {
 				'condition'		=> 'transaction_types.transaction_type_id = transactions.transaction_type_id'
 			)
 		);
+		$total_rows = $this->ledger->get_count(
+			$where,
+			array(),
+			$inner_joints
+		);
+		$offset = $this->get_pagination_offset($page, $this->_limit, $total_rows);
 
 		$data = $this->ledger->get_data(
 			$select,
@@ -54,13 +212,22 @@ class Ledger extends Admin_Controller {
 			$inner_joints,
 			array(
 				'filter'	=> 'ledger_datum_date_added',
-				'sort'		=> 'ASC'
+				'sort'		=> $sort
 			)
 		);
 
 		$ledger_data = $this->filter_ledger($data);
 
-		$this->_data['listing'] = $this->table_listing('', $ledger_data);
+		$this->_data['listing'] = $this->table_listing(
+			'', 
+			$ledger_data, 
+			$total_rows, 
+			$offset, 
+			$this->_limit, 
+			array(), 
+			2
+		);
+
 		$this->_data['title']  = "Ledger";
 		$this->set_template("ledger/list", $this->_data);
 	}

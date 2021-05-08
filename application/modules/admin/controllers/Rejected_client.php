75917ac577cf5b70a6809650d8d6d32422219e5e
claredil poso <jposoclaredil@gmail.com>
2021-05-08 15:00:22 +0800

    Added Search option in Agent request, client request and merchant request, added merchant and client Rejected logs

---
+++ application/modules/admin/controllers/Rejected_client.php
@@ -0,0 +1,76 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Rejected_client extends Admin_Controller {
+		
+	public function after_init() {
+		$this->set_scripts_and_styles();
+		
+		$this->load->model("admin/disapproval_reason_types_model", "disapproval_types");
+        $this->load->model("admin/client_disapproval_model", "client_disapproval");
+
+
+		$this->_admin_account_data = $this->get_account_data();
+	}
+
+	
+	public function index($page = 1) {
+
+        $select = array(
+			'rejected_id as "ID"',
+			'account_number as "Account No"',
+			'CONCAT(account_lname, ", ", account_fname) as "Name"',
+			'account_mobile_no as "Mobile No"',
+			'account_email_address as "Email Address"',
+			'disapproval_reason_type_description as "Reason for Rejection"',
+			'rejected_date_added as "Date"'
+		);
+
+		$inner_joints = array(
+			array(
+				'table_name'	=> 'disapproval_reason_types',
+				'condition'		=> 'disapproval_reason_types.disapproval_reason_type_id = client_rejected.disapproval_reason_type_id'
+			)
+		);
+        $where = array();
+
+		$total_rows = $this->client_disapproval->get_count(
+			$where,
+			array(),
+			$inner_joints
+		);
+
+		$offset = $this->get_pagination_offset($page, $this->_limit, $total_rows);
+
+		$data = $this->client_disapproval->get_data(
+			$select,
+			$where,
+			array(),
+			$inner_joints,
+			array(
+				'filter'	=> 'rejected_date_added',
+				'sort'		=> 'DESC'
+            ),
+            $offset,
+            $this->_limit
+		);
+
+		$this->_data['listing'] = $this->table_listing(
+			'', 
+			$data,  
+			$total_rows, 
+			$offset, 
+			$this->_limit, 
+			array(), 
+			2,
+			false,
+			false,
+			'',
+			''
+		);
+
+
+		$this->_data['title']  = "Rejected Client";
+		$this->set_template("rejected_client/list", $this->_data);
+	}
+}
